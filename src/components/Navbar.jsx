51780ce44190a770861ce00270d5d756c60af681
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-11-12 23:49:07 +0530

    update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {assets} from 'assert
+
+
+function Navbar() {
+  return <div className="absolute top-0 left-0 w-full z-10">
+    <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32">
+        <img src="" alt="" />
+    </div>
+  </div>;
+}
+
+export default Navbar;
